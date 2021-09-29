9e66e699ca7de2f539a6d37801f88193afd58e68
Afnan <afnan.qureshi.x1@gmail.com>
2021-09-29 18:13:05 +0500

    added exchange functionalities i.e make/cancel orders

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -12,7 +12,7 @@ contract Exchange{
 		mapping(address => mapping (address =>uint256)) public tokens ;
 		mapping(uint256 => _Order) public orders;
 		uint256 public orderCount;
-
+		mapping(uint256 => bool ) public orderCancelled;
 	//	address constant ETHER = address(0); // allows to store ether in tokens mapping with blank address
 
 		//fall back when ether is sent to this smart contract by mistake
@@ -24,6 +24,16 @@ contract Exchange{
 		event Deposit (address token , address user , uint256 amount, uint256 balance);
 		event Withdraw (address token , address user , uint256 amount, uint256 balance);
 		event Order(
+			uint256 id,
+			address user,
+			address tokenGet,
+			uint256 amountGet,
+			address tokenGive,
+			uint256 amountGive,
+			uint256 timestamp
+		);
+
+		event Cancel(
 			uint256 id,
 			address user,
 			address tokenGet,
@@ -98,6 +108,21 @@ contract Exchange{
 				orderCount = orderCount.add(1);
 				orders[orderCount] = _Order(orderCount ,msg.sender,  _tokenGet , _amountGet , _tokenGive, _amountGive, now);
 				emit Order(orderCount, msg.sender, _tokenGet , _amountGet , _tokenGive, _amountGive, now);
+
+			}
+
+
+			function cancelOrder(uint256 _id) public{
+				_Order storage _order = order[_id];
+				require(address(_order.user)= msg.sender);
+				require(_order.id= _id); // order must exist
+				//must be "my order"
+				
+				// must be a valid order
+
+				orderCancelled[_id] = true;
+				emit Cancel(_order.id, msg.sender, _order.tokenGet , _order.amountGet , _order.tokenGive, _order.amountGive, now);
+
 
 			}
 
