4083f6e3c3d6906b001f5e53b79a0bb4ee52ad12
Afnan <afnan.qureshi.x1@gmail.com>
2021-10-12 18:57:09 +0500

    configuring redux dev tools and showing contracts in redux env

--- src/components/App.js
+++ src/components/App.js
@@ -1,126 +1,60 @@
 import React, { Component } from 'react';
 import './App.css';
-import Web3 from "web3";
-import Token from '../abis/Token.json'
+//import Web3 from 'web3'
+import Navbar from './navbar'
+import Content from './Content'
+import { connect } from 'react-redux'
+import {
+  loadWeb3,
+  loadAccount,
+  loadToken,
+  loadExchange
+} from '../store/interactions'
+import {contractsLoadedSelector} from '../store/selectors'
 
-
+// import {accountSelector} from '../store/selectors'
 class App extends Component {
-  componentWillMount(){
-   // console.log("hi just checking")
-     this.loadBlockchainData()
+  componentWillMount() {
+    this.loadBlockchainData(this.props.dispatch)
   }
-  async loadBlockchainData(){
-    const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545')
 
-    const network = await web3.eth.net.getNetworkType();
-    const networkId = await web3.eth.net.getId();
-    //console.log('networkId',networkId);
-     //console.log('networks', networks);
-    //console.log("accounts" ,accounts )
-    //console.log('token', Token)
-    
-    const accounts = await web3.eth.getAccounts();
-    //const abi = Token.abi;
-    const networks = Token.networks
-    // console.log('abi', Token.abi)
-    // console.log('address', Token.networks[networkId].address)
-    const token = new web3.eth.Contract(Token.abi, Token.networks[networkId].address)    
-    //console.log('token', token)
-    const totalSupply = await token.methods.totalSupply().call()
-    console.log("totalSupply", totalSupply)
-  
+  async loadBlockchainData(dispatch) {
+    const web3 = await loadWeb3(dispatch)
+    const networkId = await web3.eth.net.getId()
+    await loadAccount(web3, dispatch)
+    const token = await loadToken(web3, networkId, dispatch)
+    if(!token){
+      window.alert('Token smart contract not detected on the current network.Please select another network with metamask.')
+      return    
+    }
+    const exchange = await loadExchange(web3, networkId, dispatch)
+    if(!exchange)
+    {
+      window.alert('Exchange smart contract not detected on the current network.Please select another network with metamask.')
+      return 
+    }
   }
+
   render() {
+    //console.log(this.props.account)
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-          <a className="navbar-brand" href="/#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="/#">Link 1</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/#">Link 2</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/#">Link 3</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <div className="content">
-          <div className="vertical-split">
-            <div className="card bg-dark text-white">            
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-          </div>
-          <div className="vertical">
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-          </div>
-          <div className="vertical-split">
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-          </div>
-          <div className="vertical">
-            <div className="card bg-dark text-white">
-              <div className="card-header">
-                Card Title
-              </div>
-              <div className="card-body">
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="/#" className="card-link">Card link</a>
-              </div>
-            </div>
-          </div>
-        </div>
+       <Navbar/>
+        { this.props.contractsLoaded ? <Content/> : <div className = "content"> </div> }      
+        
       </div>
+
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  //console.log('contractsLoaded?' , contractsLoadedSelector(state))
+  return {
+    contractsLoaded : contractsLoadedSelector(state)
+    //account : accountSelector(state)
+    // TODO: Fill me in...
+  }
+}
 
-
+export default connect(mapStateToProps)(App);
